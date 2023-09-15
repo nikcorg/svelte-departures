82935f83a8d46de883560f8d04e80e96fc278213
Niklas Lindgren <niklas.lindgren@iki.fi>
2023-09-15 14:12:45 +0300

    docs: explain Update fields

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,7 +10,9 @@ export type Station = [boolean, number];
 
 export interface Update {
   departures: Departure[];
+  // time offset used in departures query in minutes
   offset: number;
+  // number of departures per station
   stations: { [k: string]: number };
   updatedAt: string;
   nextUpdateAfter: string;
