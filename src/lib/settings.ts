189f57b9082489be8813449982f82dc626b3bb97
Niklas Lindgren <niklas.lindgren@iki.fi>
2023-09-15 13:53:59 +0300

    style: naming

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,30 +1,30 @@
 import { tap } from "lodash/fp";
 import { writable } from "svelte/store";
 
-const displayKey = "__display";
+const storKey = "__display";
 
 const { set, subscribe, update } = writable(new Map<string, boolean>());
 
 // the subscribable
 export const display = { subscribe };
 
-const persist = tap((state: Map<string, boolean>) => {
+const persist = tap((nextState: Map<string, boolean>) => {
   try {
-    localStorage.setItem(displayKey, JSON.stringify([...state.entries()]));
+    localStorage.setItem(storKey, JSON.stringify([...nextState.entries()]));
   } catch {}
 });
 
 // reset to the persisted state or the default state
 export const reset = () => {
   try {
-    set(new Map(JSON.parse(localStorage.getItem(displayKey) ?? "[]")));
+    set(new Map(JSON.parse(localStorage.getItem(storKey) ?? "[]")));
   } catch {}
 };
 
 // toggle the value for station
 export const toggle = (station: string) =>
-  update(x => persist(x.set(station, !(x.get(station) ?? true))));
+  update(currentState => persist(currentState.set(station, !(currentState.get(station) ?? true))));
 
 // Set if Not eXists
 export const setnx = (station: string, v: boolean) =>
-  update(x => persist(x.set(station, x.get(station) ?? v)));
+  update(currentState => persist(currentState.set(station, currentState.get(station) ?? v)));
